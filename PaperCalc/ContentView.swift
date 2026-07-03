384201ead963144ba10c65844c9875eff6d5c5b5
xiaobo.chen <xiaobo.chen@ucloud.cn>
2026-07-03 15:56:39 +0800

    Initial Commit

---
+++ PaperCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaperCalc
+//
+//  Created by xiaobo.chen on 2026/7/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaperCalc/PaperCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperCalcApp.swift
+//  PaperCalc
+//
+//  Created by xiaobo.chen on 2026/7/3.
+//
+
+import SwiftUI
+
+@main
+struct PaperCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
